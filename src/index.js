5207f83d7d38c61ea454f563a4794d91f905ffbb
JacyDee274 <jacydee274@gmail.com>
2023-11-17 19:40:19 +0100

    Added API Call

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#weathertemp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-app-city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "c2dtd96d439e4038fe94409oc4ab6feb";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-app-city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-alert("hello");
+searchCity("Bremen");
